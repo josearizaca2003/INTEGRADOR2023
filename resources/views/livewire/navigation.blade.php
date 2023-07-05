77e02c44dd4ed0e924b0db118718108f0ce5d5f0
josearizaca2003 <josequispearizaca2003@gmail.com>
2023-07-05 13:40:43 -0500

    dasdagdfs

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,7 +1,9 @@
-<nav class="bg-gray-800">
+<nav class="bg-black">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="relative flex h-16 items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <div class="bg-cover bg-no-repeat bg-center h-screen" style="background-image: url('URL_DE_LA_IMAGEN')">
+              </div>
           <!-- Mobile menu button-->
           <button type="button" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
@@ -102,3 +104,4 @@
       </div>
     </div>
   </nav>
+
